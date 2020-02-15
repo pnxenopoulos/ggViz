a2ea4c54212677168d94b9b2408c917afd0607e6
pnxenopoulos <pnxenopoulos@gmail.com>
2020-02-15 14:27:20 -0500

    initial ggviz commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ggviz',
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+	'flask_sqlalchemy',
+	'flask_testing'
+    ],
+)
---
+++ tests/conftest.py
@@ -0,0 +1,25 @@
+import os
+import tempfile
+
+import pytest
+from ggviz import create_app
+
+
+@pytest.fixture
+def app():
+
+    app = create_app({
+        'TESTING': True,
+    })
+
+    yield app
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
