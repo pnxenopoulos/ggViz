68021d5581d116c7d533badff90a16b6c1afb8be
pnxenopoulos <pnxenopoulos@gmail.com>
2020-02-15 16:45:34 -0500

    fixed favicon path

--- ggviz/__init__.py
+++ ggviz/__init__.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template
 
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True, static_folder="static", template_folder="templates")
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
